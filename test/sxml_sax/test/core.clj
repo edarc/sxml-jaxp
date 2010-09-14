b2baf7b0047f92a1bce7166695426552dc383d6b
Kyle Schaffrick <kyle@raidi.us>
2010-09-14 14:47:47 -0400

    Add tests for normalization and simplification of SXML forms.

--- test/sxml_sax/test/core.clj
+++ test/sxml_sax/test/core.clj
@@ -2,5 +2,67 @@
   (:use [sxml-sax.core] :reload)
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest normalization "normalization of SXML forms"
+  (testing "non-markup"
+    (is (= (normalize "foo") "foo")))
+  (testing "empty elements"
+    (are [in] (= (normalize in) [:t {}])
+      :t     
+      [:t]   
+      [:t {}]))
+  (testing "non-empty elements with no attributes"
+    (are [in] (= (normalize in) [:t {} "foo"])
+      [:t "foo"]
+      [:t {} "foo"])
+    (are [in] (= (normalize in) [:t {} "foo" "bar"])
+      [:t "foo" "bar"]
+      [:t {} "foo" "bar"]))
+  (testing "irreducible element forms"
+    (are [form] (= (normalize form) form)
+      [:t {:foo "bar"}]
+      [:t {:foo "bar"} "foo"]
+      [:t {:foo "bar"} "foo" "bar"]))
+  (testing "recursive normalization of elements"
+    (are [in] (= (normalize in) [:t1 {} [:t2 {}]])
+      [:t1 :t2]
+      [:t1 {} :t2]
+      [:t1 [:t2]]
+      [:t1 {} [:t2]]
+      [:t1 [:t2 {}]]
+      [:t1 {} [:t2 {}]])
+    (are [in] (= (normalize in) [:t1 {} [:t2 {}] [:t3 {}]])
+      [:t1 :t2 :t3]
+      [:t1 {} :t2 :t3])))
+
+(deftest simplification "simplification of SXML forms"
+  (testing "non-markup"
+    (is (= (simplify "foo") "foo")))
+  (testing "empty elements"
+    (are [in] (= (simplify in) :t)
+      :t
+      [:t]
+      [:t {}]))
+  (testing "non-empty elements with no attributes"
+    (are [in] (= (simplify in) [:t "foo"])
+      [:t "foo"]
+      [:t {} "foo"])
+    (are [in] (= (simplify in) [:t "foo" "bar"])
+      [:t "foo" "bar"]
+      [:t {} "foo" "bar"]))
+  (testing "irreducible element forms"
+    (are [form] (= (simplify form) form)
+      [:t {:foo "bar"}]
+      [:t {:foo "bar"} "foo"]
+      [:t {:foo "bar"} "foo" "bar"]))
+  (testing "recursive simplification of elements"
+    (are [in] (= (simplify in) [:t1 :t2])
+      [:t1 :t2]
+      [:t1 {} :t2]
+      [:t1 [:t2]]
+      [:t1 {} [:t2]]
+      [:t1 [:t2 {}]]
+      [:t1 {} [:t2 {}]])
+    (are [in] (= (simplify in) [:t1 :t2 :t3])
+      [:t1 :t2 :t3]
+      [:t1 {} :t2 [:t3]]
+      [:t1 {} :t2 [:t3 {}]])))
