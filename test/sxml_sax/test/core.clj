dae3d2cdcf8b2aba06a980a54beb59038fc77a1b
Kyle Schaffrick <kyle@raidi.us>
2010-09-15 11:26:42 -0400

    Add tests for SAX event generation.

--- test/sxml_sax/test/core.clj
+++ test/sxml_sax/test/core.clj
@@ -66,3 +66,51 @@
       [:t1 :t2 :t3]
       [:t1 {} :t2 [:t3]]
       [:t1 {} :t2 [:t3 {}]])))
+
+(deftest event-generation "generation of SAX events from SXML"
+  (is (= (sax-event-seq "foo")
+         [[:text-node "foo"]])
+      "non-markup")
+  (is (= (sax-event-seq :t)
+         [[:start-element :t {}]
+          [:end-element :t]])
+      "empty root element")
+  (is (= (sax-event-seq [:root :inner])
+         [[:start-element :root {}]
+          [:start-element :inner {}]
+          [:end-element :inner]
+          [:end-element :root]])
+      "single nested element")
+  (is (= (sax-event-seq [:root :bob [:alice :eve] :mallory])
+         [[:start-element :root {}]
+          [:start-element :bob {}]
+          [:end-element :bob]
+          [:start-element :alice {}]
+          [:start-element :eve {}]
+          [:end-element :eve]
+          [:end-element :alice]
+          [:start-element :mallory {}]
+          [:end-element :mallory]
+          [:end-element :root]])
+      "multiple nested elements")
+  (is (= (sax-event-seq [:root {:foo "bar"}])
+         [[:start-element :root {:foo "bar"}]
+          [:end-element :root]])
+      "empty element with attributes")
+  (is (= (sax-event-seq [:root [:bob {:foo "bar"}]])
+         [[:start-element :root {}]
+          [:start-element :bob {:foo "bar"}]
+          [:end-element :bob]
+          [:end-element :root]])
+      "nested element with attributes")
+  (is (= (sax-event-seq [:root "foo"])
+         [[:start-element :root {}]
+          [:text-node "foo"]
+          [:end-element :root]])
+      "nested text node")
+  (is (= (sax-event-seq [:root {:xmlns:foo "foo"}])
+         [[:start-prefix :foo "foo"]
+          [:start-element :root {:xmlns:foo "foo"}]
+          [:end-element :root]
+          [:end-prefix :foo]])
+      "namespace declaration"))
