76e6aa089c5d0488bd6226df240cd4a65a2a881a
Kyle Schaffrick <kyle@raidi.us>
2010-09-19 00:03:59 -0400

    Add github URL to project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject
   sxml-sax "0.1.0"
   :description "Tools for using SXML-inspired XML representations with SAX2."
+  :url "http://github.com/edarc/sxml-sax"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[vimclojure/server "2.2.0-SNAPSHOT"]])
