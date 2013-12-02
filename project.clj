69fbae58401b6b15abfadd45bebe9233568ec085
Kyle Schaffrick <kyle@raidi.us>
2013-12-02 02:03:18 -0800

    Release 0.2.0, why not.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sxml-jaxp "0.1.0"
+(defproject sxml-jaxp "0.2.0"
   :description "Tools for using SXML-inspired XML representations with JAXP."
   :url "http://github.com/edarc/sxml-jaxp"
   :dependencies [[org.clojure/clojure "1.5.1"]])
